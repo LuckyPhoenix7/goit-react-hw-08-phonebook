78267d656cc01a963a6bab9f872fbb001ee76249
LuckyPhoenix7 <a.lubashov@gmail.com>
2024-02-02 01:41:25 +0100

    HW#8 v3.2

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import { StyledInput, StyledLabel } from './Filter.styled';
+// import { FilterContainer, FilterInput } from './Filter.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from 'redux/filterSlice';
+
+import {
+  Box,
+  FormControl,
+  Input,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { selectFilter } from 'redux/selectors';
 
 export const Filter = () => {
   const dispatch = useDispatch();
   const filter = useSelector(selectFilter);
 
+  const handleChange = event => {
+    dispatch(setFilter(event.target.value));
+  };
+
+  const filterBgColor = useColorModeValue('teal.200', 'teal.700');
+
   return (
-    <StyledLabel htmlFor="filter">
-      Find contact by Name
-      <StyledInput
-        value={filter}
-        onChange={evt => dispatch(setFilter(evt.target.value))}
-      />
-    </StyledLabel>
+    <Box
+      p={6}
+      borderWidth="1px"
+      borderRadius="md"
+      boxShadow="base"
+      bg={filterBgColor}
+    >
+      <Text mb={2} fontSize="md" fontWeight="bold">
+        Find contacts by name
+      </Text>
+      <FormControl>
+        <Input
+          type="text"
+          name="filter"
+          placeholder="Enter name to find"
+          value={filter}
+          onChange={handleChange}
+        />
+      </FormControl>
+    </Box>
   );
 };

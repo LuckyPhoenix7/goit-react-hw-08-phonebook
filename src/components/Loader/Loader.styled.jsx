d21917e52775c165d5e5440445efe7fbc6a7a8e6
LuckyPhoenix7 <a.lubashov@gmail.com>
2024-02-02 02:20:04 +0100

    HW#8 v4.0

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,7 +1,19 @@
+// import styled from 'styled-components';
+
+// export const LoaderContainer = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
 import styled from 'styled-components';
 
 export const LoaderContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 100vh; // Full viewport height
+  background-color: ${p =>
+    p.theme.colors.background}; // Use the background color from the theme
 `;

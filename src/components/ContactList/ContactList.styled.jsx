415f79fe403059913f20d499eddf6c41e9bb64b6
LuckyPhoenix7 <a.lubashov@gmail.com>
2024-02-02 02:23:45 +0100

    HW#8 v4.1

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -1,19 +1,3 @@
-// import styled from 'styled-components';
-
-// export const List = styled.ul`
-//   list-style-type: none;
-//   padding: 0;
-//   width: 560px;
-//   margin: 20px auto;
-// `;
-
-// export const Contact = styled.li`
-//   background-color: ${p => p.theme.colors.listItemBg};
-//   margin-bottom: 8px;
-//   padding: 10px;
-//   border-radius: 5px;
-// `;
-
 import styled from 'styled-components';
 
 export const List = styled.ul`
@@ -21,14 +5,11 @@ export const List = styled.ul`
   padding: 0;
   width: 560px;
   margin: 20px auto;
-  background-color: ${p => p.theme.colors.background}; // Светлый бежевый
 `;
 
 export const Contact = styled.li`
-  background-color: ${p => p.theme.colors.primary}; // Пыльная роза
-  color: ${p => p.theme.colors.text}; // Темно-коричневый
+  background-color: ${p => p.theme.colors.listItemBg};
   margin-bottom: 8px;
   padding: 10px;
   border-radius: 5px;
-  border: 1px solid ${p => p.theme.colors.secondaryText}; // Бледно-розовый
 `;
